5f8be00e2f2ac04e31bacc03753d4e665ee4e5ce
alisonharman <alison_harman@sympatico.ca>
2022-08-30 21:52:55 -0400

    encrypts whole strings given key

--- caesar.js
+++ caesar.js
@@ -1,5 +1,34 @@
-const encrypt = function(plaintext, key) {
-    // IMPLEMENT ME
+
+
+const shiftCharacter = function(plaintext, key) {
+    // do not shift if letter is blank" "
+    if (plaintext === " ") {
+      return plaintext;
+    }
+
+    // need to check if key and letter will take it outside the range of the lower-case unicode alphabet
+    let index = plaintext.charCodeAt(0);
+    let newIndex = index + key;
+    // wraps index if larger than 122 (which equals z)
+    if (newIndex > 122) {
+      newIndex = newIndex - 122 + 96;
+    }
+    // wraps index if smaller than 97 (which equals a)
+    if (newIndex < 97) {
+      newIndex =  122 - (96 - newIndex); 
+    }
+
+    const char = String.fromCharCode(newIndex)
+    return char;
   };
   
+  const encrypt = function(plaintext, key) {
+    let encryptedString = "";
+    for (let c of plaintext) {
+      const newChar = shiftCharacter(c, key);
+      encryptedString += newChar;
+    }
+    return encryptedString;
+  }
+
   module.exports = { encrypt };
